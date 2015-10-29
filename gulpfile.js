c63318790cbcaaccee5e7a44dfefefab7dc5a4d8
Hideyuki Motoo <hideyuki.motoo@gmail.com>
2015-10-29 15:21:28 +0900

    browserSync導入

--- gulpfile.js
+++ gulpfile.js
@@ -8,26 +8,26 @@ var gulp = require( "gulp" );
 
 var autoprefixer = require( "gulp-autoprefixer" );
 var cached		 = require( "gulp-cached" );
-var browsersync	 = require( "browser-sync" );
+var browserSync	 = require( "browser-sync" );
 var imagemin	 = require( "gulp-imagemin" );
 var plumber		 = require( "gulp-plumber" );
 var pngquant	 = require( "imagemin-pngquant");
 var sass		 = require( "gulp-ruby-sass" );
 var uglify		 = require( "gulp-uglify" );
 
-gulp.task( "default", function () {
+gulp.task( "default", [ "browser-sync" ], function () {
 	gulp.watch( "./assets/src/js/**/*.js", [ "js" ] );
 	gulp.watch( "./assets/src/sass/**/*.scss", [ "sass" ] );
-	gulp.watch( [ "./**/*.html", "./**/*.css", "./**/*.js" ], function () {
-		browserSync.reload();
-	});
+	gulp.watch( "./**/*.html", [ "browser-reload" ] );
+	gulp.watch( "./**/*.css",  [ "browser-reload" ] );
+	gulp.watch( "./**/*.js",   [ "browser-reload" ] );
 } );
 
 gulp.task( "js", function () {
 	return gulp.src( "./assets/src/js/**/*.js" )
 		.pipe( uglify() )
 		.pipe( gulp.dest( "./assets/js" ) );
-});
+} );
 gulp.task( "sass", function () {
 	return sass( "./assets/src/sass/", { compass: true, style: 'expanded' })
 		.pipe( plumber() )
@@ -35,15 +35,16 @@ gulp.task( "sass", function () {
 		.pipe( autoprefixer() )
 		.pipe( gulp.dest( "./assets/css" ) );
 } );
+gulp.task( "browser-reload", function () {
+	browserSync.reload();
+} );
 gulp.task( "browser-sync", function () {
 	browserSync( {
 		server: {
 			baseDir: "./"
-		},
-		// external 指定すると、実行時のアクセスURLがIPアドレス指定側で開くことができる
-		open: 'external',
+		}
 	});
-});
+} );
 
 /* defaultには追加しない（だって時間的コスト大きいやん） */
 gulp.task( "imagemin", function () {
