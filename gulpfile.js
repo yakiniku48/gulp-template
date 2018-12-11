acc66a1b0e56fe32c8668d85991b29db4ed12d5f
Hideyuki Motoo <hideyuki.motoo@gmail.com>
2018-12-11 15:23:33 +0900

    simplify

--- gulpfile.js
+++ gulpfile.js
@@ -1,58 +1,20 @@
 /*!
  * guplfile template
- * Copyright 2015 Hideyuki MOTOO
+ * Copyright 2019 Hideyuki MOTOO
  * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
  */
 
-var gulp = require( "gulp" );
-
-var autoprefixer = require( "gulp-autoprefixer" );
-var cached		 = require( "gulp-cached" );
-var browserSync	 = require( "browser-sync" );
-var imagemin	 = require( "gulp-imagemin" );
-var plumber		 = require( "gulp-plumber" );
-var pngquant	 = require( "imagemin-pngquant");
-var sass		 = require( "gulp-ruby-sass" );
-var uglify		 = require( "gulp-uglify" );
-
-gulp.task( "default", [ "browser-sync" ], function () {
-	gulp.watch( "./assets/src/js/**/*.js", [ "js" ] );
-	gulp.watch( "./assets/src/sass/**/*.scss", [ "sass" ] );
-	gulp.watch( "./**/*.html", [ "browser-reload" ] );
-	gulp.watch( "./**/*.css",  [ "browser-reload" ] );
-	gulp.watch( "./**/*.js",   [ "browser-reload" ] );
-} );
-
-gulp.task( "js", function () {
-	return gulp.src( "./assets/src/js/**/*.js" )
-		.pipe( uglify() )
-		.pipe( gulp.dest( "./assets/js" ) );
-} );
-gulp.task( "sass", function () {
-	return sass( "./assets/src/sass/*.scss", { compass: true, style: "expanded" } )
-		.pipe( plumber() )
-		.pipe( cached( "sass" ) )
-		.pipe( autoprefixer() )
-		.pipe( gulp.dest( "./assets/css" ) );
-} );
-gulp.task( "browser-reload", function () {
-	browserSync.reload();
-} );
-gulp.task( "browser-sync", function () {
-	browserSync( {
-		server: {
-			baseDir: "./"
-		}
-	});
-} );
-
-/* defaultには追加しない（だって時間的コスト大きいやん） */
-gulp.task( "imagemin", function () {
-	return gulp.src( "./assets/src/img/**/*" )
-	.pipe( imagemin( {
-		progressive: true,
-		svgoPlugins: [ { removeViewBox: false } ],
-		use: [ pngquant( { quality: '60-80' } ) ]
-	} ) )
-	.pipe( gulp.dest( "./assets/img" ) );
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+ 
+gulp.task( "default", function () {
+  return gulp.watch( "assets/src/scss/**/*.scss", function () {
+    return gulp.src( "assets/src/scss/style.scss" )
+      .pipe(
+        sass( {
+          outputStyle: "expanded"
+        } ).on( "error", sass.logError )
+      )
+      .pipe( gulp.dest( "assets/css" ) );
+  } );
 });
